bdb2099b21ce5fabad1ce9a5efd5987842607b9a
stygian-96 <54054289+stygian-96@users.noreply.github.com>
2021-06-17 19:16:07 +0530

    Gender Neutral Text Prompts

--- inclusive-web/src/components/Form/Controls/FormInput.js
+++ inclusive-web/src/components/Form/Controls/FormInput.js
@@ -4,8 +4,13 @@ import {
     Grid,
     FormControl,
     Typography,
-    TextField
+    TextField,
+    Tooltip,
+    ClickAwayListener
 } from '@material-ui/core'
+import {
+    Error
+} from '@material-ui/icons'
 import clsx from 'clsx'
 
 const useStyles = makeStyles(theme => ({
@@ -57,23 +62,73 @@ const useStyles = makeStyles(theme => ({
         "&:focus-within .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
             border: "2px solid #f44336 !important"
         },
+    },
+    // tooltip
+    customWidth: {
+        maxWidth: "360px",
+        fontSize: "12px"
+    },
+    icon: {
+        color: "#f44336"
     }
 }))
 
-function FormInput({value, handleChange, name, label, error=null, multiline, small, integer, placeholder="type here.."}) {
+function FormInput({value, handleChange, name, label, error=null, multiline, small, integer, placeholder="type here..", promptValue = ""}) {
     const classes = useStyles()
+
+    const [open, setOpen] = React.useState(false)
+
+    const handleTooltipClose = () => {
+        setOpen(false);
+    }
+
+    const handleTooltipOpen = () => {
+        setOpen(true);
+    }
+
+    React.useEffect(() => {
+        setOpen(promptValue.length ? true : false)
+        console.log("changes")
+    }, [promptValue])
 
     return (
         <Grid item container direction="column" className={clsx({[classes.smallForm]: small})}>
-            <Typography 
-                variant="h6" 
-                style={{fontSize: "14px", margin: "12px 0 8px 0", letterSpacing: "0.4px"}}
-                className={clsx({
-                    [classes.smallForm]: small
-                })}
-            >
-                {label}
-            </Typography>
+            <Grid item container alignItems='center'>
+                <Typography 
+                    variant="h6" 
+                    style={{fontSize: "14px", margin: "12px 0 8px 0", letterSpacing: "0.4px", flex: "1 1"}}
+                    className={clsx({
+                        [classes.smallForm]: small
+                    })}
+                >
+                    {label}
+                </Typography>
+                {promptValue.length ? 
+                    <ClickAwayListener onClickAway={handleTooltipClose}>
+                        <Grid>
+                            <Tooltip
+                                PopperProps={{
+                                disablePortal: true,
+                                }}
+                                onClose={handleTooltipClose}
+                                open={open}
+                                disableFocusListener
+                                disableHoverListener
+                                disableTouchListener
+                                title={promptValue}
+                                placement='top-end'
+                                classes={{
+                                    tooltip: classes.customWidth
+                                }}
+                            >
+                                <Error className= {classes.icon} onClick={handleTooltipOpen}/>
+                            </Tooltip>
+                        </Grid>
+                    </ClickAwayListener>
+                :
+                    null
+                }
+            </Grid>
             <FormControl 
                 className={clsx(classes.root,{
                     [classes.smallForm]: small
@@ -82,7 +137,7 @@ function FormInput({value, handleChange, name, label, error=null, multiline, sma
                 <TextField
                     className={clsx(classes.input,{
                         [classes.singleLineInput]: !multiline,
-                        [classes.error]: error ? true : false
+                        [classes.error]: (error || promptValue.length) ? true : false
                     })}
                     value={value}
                     variant="outlined"
@@ -92,7 +147,7 @@ function FormInput({value, handleChange, name, label, error=null, multiline, sma
                     autoComplete="off"
                     multiline={multiline}
                     {
-                        ...(error && {
+                        ...((error || promptValue.length) && {
                                         error: true,
                                         helperText: error            
                                     })
